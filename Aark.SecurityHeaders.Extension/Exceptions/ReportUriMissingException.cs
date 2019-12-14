201d7e532bf7d1bf186f33b8db6f15f802e6778f
Edouard Biton <edouard.biton@outlook.com>
2019-12-14 11:10:13 +0100

    Added report-only header by default for CSP.

---
+++ Aark.SecurityHeaders.Extension/Exceptions/ReportUriMissingException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Aark.SecurityHeaders.Extension.Exceptions
+{
+    /// <summary>
+    /// Occurs when the report uri is mandatory but not available.
+    /// </summary>
+    [Serializable]
+    public class ReportUriMissingException : Exception
+    {
+        /// <summary>
+        /// Create a new ReportUriMissingException.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Ne pas passer de littéraux en paramètres localisés", Justification = "<En attente>")]
+        public ReportUriMissingException()
+            : base("The report URI is missing. Call AddReportUri or assign the value false to the reportOnly parameter")
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public ReportUriMissingException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public ReportUriMissingException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected ReportUriMissingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
